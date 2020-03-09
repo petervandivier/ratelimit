da6e2d39bfd5d497a7ed733c3828aca9dbad1bdf
petervandivier <petervandivier@gmail.com>
2020-03-09 09:18:37 +0000

    revert node-type UQ w/ comments

--- db/tables/node_type_cap.sql
+++ db/tables/node_type_cap.sql
@@ -4,5 +4,7 @@ create table node_type_cap (
     type_id int not null references "type" (id),
     cap_id  int not null references cap (id),
 -- ¿it doesn't make sense to allow multiple caps per node-type, right?
-    unique (node_id, type_id) 
+-- right... i want to allow multiple caps with different labels & evaluate
+-- per unique `cap.span`. 🤔 think i need to fix the normalization here
+    unique (node_id, type_id, cap_id) 
 );
